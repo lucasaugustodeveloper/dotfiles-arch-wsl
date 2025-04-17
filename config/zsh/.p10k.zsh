ed520f8fabdc89b0da4e0d395eaff9f3c9f7c41b
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2025-04-17 14:35:49 -0300

    ♻️ Remove time of show in terminal

--- config/zsh/.p10k.zsh
+++ config/zsh/.p10k.zsh
@@ -104,7 +104,7 @@
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     # cpu_arch              # CPU architecture
-    time                    # current time
+    # time                    # current time
     battery                 # internal battery
     # =========================[ Line #2 ]=========================
     newline                 # \n
